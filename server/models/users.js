4195433a804d1bcae48434a24bdff07bf118f4cd
kr3110 <n.nkr3110@gmail.com>
2018-03-24 11:19:04 -0400

    making email uniq

--- server/models/users.js
+++ server/models/users.js
@@ -3,15 +3,18 @@ const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true
+  },
+  password: String,
+});
 
-        email: String,
-        password: String,
-  });
-  userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(), null);
-  };
-  userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-  };
+userSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(), null);
+};
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
-  module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model('users', userSchema);
