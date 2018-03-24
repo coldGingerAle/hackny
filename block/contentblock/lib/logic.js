84127ca646d742ac0c4c8f10254ca5c47ffa0dc3
kr3110 <n.nkr3110@gmail.com>
2018-03-24 19:14:46 -0400

    block rest api deploy

---
+++ block/contentblock/lib/logic.js
@@ -0,0 +1,27 @@
+'use strict';
+/**
+ * Write your transction processor functions here
+ */
+
+ /**
+ * Sample transaction
+ * @param {test.Rating } rating
+ * @transaction
+ */
+
+ function Rating(Rating) {
+   Rating.to.peopleRated += 1;
+   Rating.to.rated +=  rating.RatingAmount ;
+   
+
+   return getAssetRegistry('test.Rating')
+   .then (function (rating) {
+   return assetRegistry.update(rating.from);
+   })
+   .then (function () {
+   return getAssetRegistry('test.Rating');
+   })
+   .then(function (rating) {
+   return assetRegistry.update(rating.to);
+   });
+   }
